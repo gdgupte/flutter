9314f2a8717990a6c8df716318e7aab336e7d7a0
Gaurav Gupte <gdgupte@gmail.com>
2019-09-10 00:07:50 +0530

    implement questions map & add to answer widget

--- lib/answer.dart
+++ lib/answer.dart
@@ -2,16 +2,23 @@ import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
   final Function selectHandler;
+  final String answerText;
 
-  Answer(this.selectHandler);
+  Answer(this.selectHandler, this.answerText);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
+      padding: EdgeInsets.symmetric(
+        horizontal: 30.0,
+      ),
       child: RaisedButton(
+        // minWidth: 10.0,
         color: Colors.blue,
-        child: Text('Answer 1'),
+        textColor: Colors.white,
+        padding: EdgeInsets.all(1),
+        child: Text(answerText),
         onPressed: selectHandler,
       ),
     );
